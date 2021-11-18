2d920b358b462fd074ca5b29a0efed1dd73c9b59
RaDioDa <teamfeag418@gmail.com>
2021-11-18 21:38:19 +0300

    Добавьте файлы проекта.

---
+++ SR_lab_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SR_lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fail1 = 2;
+            int fail2 = 86;
+            int work = 3 * 3600;
+            int m = (fail1 + fail2) / 2;
+            double tt = 0;
+            double t = 0;
+            double k = 0.005;
+
+            for(int i = 0; t < work; i++)
+            {
+                t = (1 / (k * m)) * Math.Exp(i / (m * (k * m)));
+                tt += t;
+            }
+
+            Console.WriteLine(tt/3600);
+        }
+    }
+}
